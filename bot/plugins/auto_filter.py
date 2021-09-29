d337916c7b282156dd1a503011bd90918e5858e7
Predator <87071127+Predatorff@users.noreply.github.com>
2021-09-29 22:47:38 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -208,7 +208,7 @@ async def auto_filter(bot, update):
         reply_markup = InlineKeyboardMarkup(result[0])
 
         try:
-            await bot.send_message(
+            msg=await bot.send_message(
                 chat_id = update.chat.id,
                 text=f"📂𝗧𝗼𝘁𝗮𝗹 𝗙𝗶𝗹𝗲𝘀 {(len_results)} \n📁𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲:- <code>{query}</code>\n\n🔆𝗖𝗜𝗡𝗘𝗠𝗔 𝗪𝗢𝗥𝗟𝗗🔆\n<a href=https://t.me/joinchat/5qjx72HxFXA0YWZl >🗣️𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>",
                 reply_markup=reply_markup,
@@ -216,6 +216,8 @@ async def auto_filter(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
+            await asyncio.sleep(300)
+            await msg.delete()
 
         except ButtonDataInvalid:
             print(result[0])
