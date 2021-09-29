fc9c9220087e1e0541cc462f61f23feb69a29813
Predator <87071127+Predatorff@users.noreply.github.com>
2021-09-29 21:58:11 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -126,8 +126,15 @@ async def auto_filter(bot, update):
             )
         
     else:
-        return # return if no files found for that query
-    
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=f"Your text",
+            parse_mode="html",
+            
+    reply_to_message_id=update.message_id)
+        return
+
+
 
     if len(results) == 0: # double check
         return
