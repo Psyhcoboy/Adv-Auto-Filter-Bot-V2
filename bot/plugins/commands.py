432492411fec0e555eaa4e412b460910e65f827a
Predator <87071127+Predatorff@users.noreply.github.com>
2021-09-30 15:58:53 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -209,4 +209,21 @@ async def about(bot, update):
 @Client.on_message(filters.command(["google"]) & filters.private, group=1)
 async def about(bot, update):
     buttons = [[
-        InlineKeyboardButton("serch 
+        InlineKeyboardButton('serch on google', url='http://google.com'),
+        InlineKeyboardButton('Contact admin', url='http://t.me/cw_access_bot'),
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons) 
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.GOOGLE_TEXT,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    ) 
+
+    
+    
+        
+   
