0b2fc37c1042031efe6ed2cad9b2c9a58e3b1fc6
Predator <87071127+Predatorff@users.noreply.github.com>
2021-09-30 22:59:52 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -126,7 +126,14 @@ async def auto_filter(bot, update):
             )
         
     else:
-        return # return if no files found for that query
+        await update.reply_photo(
+            photo="Link",
+            caption="Your Text",
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(text="📞Contact admin", url="http://t.me/cw_access_bot"
+                                     )]])
+
+
     
 
     if len(results) == 0: # double check
