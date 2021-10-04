13be033db8a4e3e0f264b09c46d5a619fd757234
Predator <87071127+Predatorff@users.noreply.github.com>
2021-10-04 12:36:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -214,8 +214,7 @@ async def auto_filter(bot, update):
                 parse_mode="html",
                 reply_to_message_id=update.message_id
             )
-            await asyncio.sleep(300)
-            await msg.delete(10)
+            
 
         except ButtonDataInvalid:
             print(result[0])
