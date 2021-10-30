4d55b8d6dc99d1fcd9f8b5c1a58b0bb2b180366f
Psyhcoboy <93420069+Psyhcoboy@users.noreply.github.com>
2021-10-30 12:10:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,18 +6,19 @@ class Translation(object):
 
     START_TEXT = """<b>Hey {}!!</b>
 
-<b>Am an auto filter bot exclusively maded for <a href="https://t.me/Cinema_world_officiel">cinema world</a>\n\nAnd don't try to add me in your group. Am totally different😌</b>
+<b>Don't try to spam here ask movies on <a href="https://t.me/Cinema_world_officiel">cinema world</a>
  """   
     HELP_TEXT = """
-<b>Sorry I can't help you😏</b>
+<b>No gonna help you</b>
 """
 
 
     
-    ABOUT_TEXT = """    <b><i>Bot Info</i></b>
+    ABOUT_TEXT = """   <b><i><u>Bot Info</i></b></u>
     <b>Bot Nmae</b>: <b><i><a href="t.me/cw_professor_bot">◢◤мαѕтєя◢◤¹</a></i></b>
-    <b>Creator</b>: <b><i><a href="https://t.me/cw_perfect">perfect</a></i></b>
-    <b>Server</b>: <i>Heroko</i>
+    <b>Creator</b>:  <b><i><a href="https://t.me/cw_perfect">perfect</a></i></b>
+    <b>owner</b>  :  <b><i><a href="https://t.me/cw_perfect">perfect</a></i></b>
+    <b>Server</b>:   <i>Heroko</i>
     -------------------------
     <b>Note</b>: നിങ്ങൾക്ക് എന്നെ വേറെ ഒരിടത്തും ഉപയോഗിക്കാൻ പറ്റൂല വേണേൽ നിങ്ങൾക്ക് Start,Help,About ഒക്കെ അടിച്ച് കളിക്കാം അത്ര മാത്രം..ഇത്രേ എനിക്ക് ചെയ്ത് തരാൻ പറ്റൂ🤭
     """
